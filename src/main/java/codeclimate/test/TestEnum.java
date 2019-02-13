a90d118e82ab0c73fe9afbe42c1e3d1959b2a67a
Jonathan Leitschuh <Jonathan.Leitschuh@gmail.com>
2019-02-12 19:22:54 -0500

    Add a test classpath entry

--- src/main/java/codeclimate/test/TestEnum.java
+++ src/main/java/codeclimate/test/TestEnum.java
@@ -7,4 +7,15 @@ public enum TestEnum {
     static {
         System.out.println("RCE!");
     }
+
+    void comparingThem() {
+        TestEnum first = TestEnum.TEST_ME;
+        TestEnum second = TestEnum.TEST_ME;
+
+        if (first == second) {
+            if ("a" == "b") {
+
+            }
+        }
+    }
 }
--- test-command.sh
+++ test-command.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-~/pmd-bin-6.7.0/bin/run.sh pmd -R https://raw.githubusercontent.com/JLLeitschuh/codeclimate-test/master/my-custom.xml -f codeclimate  -d /src
+~/pmd-bin-6.7.0/bin/run.sh pmd -R https://raw.githubusercontent.com/JLLeitschuh/codeclimate-test/master/my-custom.xml -f codeclimate -auxclasspath https://raw.githubusercontent.com/JLLeitschuh/codeclimate-test/master/codeclimate-test.jar -d /Users/jonathanleitschuh/git_repos/codeclimate-test/src
